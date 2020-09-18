78055db1efa070294f2badc0c7e930713ecec62e
wuyt09 <yangsong3@mail.sysu.edu.cn>
2020-09-19 02:01:04 +0800

    200919

---
+++ TP_coldbias/model/a20200910/cfram20200917/run.sh
@@ -0,0 +1,13 @@
+
+#varname=("warm" "albedo" "cloud" "co2" "drdt" "o3" "solar" "t" "wv")
+varname=("albedo" "co2" "drdt" "o3" "solar")
+size=${#varname[*]}
+for ((i=0; i<${size}; i++))
+do
+    nohup ./${varname[i]}.out > ${varname[i]}.txt &
+done
+
+
+
+
+
