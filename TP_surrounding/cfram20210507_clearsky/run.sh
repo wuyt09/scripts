52722e3a21576dfdb5c3cd7e885a44323ead6f2b
wuyt09 <yangsong3@mail.sysu.edu.cn>
2021-05-08 02:01:04 +0800

    210508

---
+++ TP_surrounding/cfram20210507_clearsky/run.sh
@@ -0,0 +1,13 @@
+
+varname=("albedo" "cloud" "co2" "drdt" "o3" "solar" "ta" "wv")
+#varname=("albedo" "co2" "drdt" "o3" "solar")
+size=${#varname[*]}
+for ((i=0; i<${size}; i++))
+do
+    nohup ./1-${varname[i]}.out > 1-${varname[i]}.txt &
+done
+
+
+
+
+
