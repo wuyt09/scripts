ff28515993ad709760a7101d3055b0f1fb1cfba2
wuyt09 <yangsong3@mail.sysu.edu.cn>
2021-08-01 02:01:07 +0800

    210801

--- Antarctic_melt/8283/soft.sh
+++ Antarctic_melt/8283/soft.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
-for (( i = 3; i < 63; i++ )); do
-	ln -s /home/yangsong3/huxm/antarctic_melt/base_no_cloud_out_$i.dat
+for (( i = 1; i < 63; i++ )); do
+	# ln -s /home/yangsong3/huxm/antarctic_melt/base_no_cloud_out_$i.dat
+	# ln -s /home/yangsong3/huxm/antarctic_melt/baseline_radranc_$i.grd
+	ln -s /home/yangsong3/huxm/antarctic_melt/baseline_radranc_$i.grd
+
 done
--- Antarctic_melt/9192/soft.sh
+++ Antarctic_melt/9192/soft.sh
@@ -1,17 +1,22 @@
 #!bin/bash
 
-ln -s /home/yangsong3/huxm/antarctic_melt/drdt_ranc_1.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/cc_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/ciwc_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/clwc_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/hus_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/huss_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/o3_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/ps_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/ssrd_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/ssru_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/ta_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/tisr_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/ts_base.dat
-ln -s /home/yangsong3/huxm/antarctic_melt/base_no_cloud_out_1.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/drdt_ranc_1.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/cc_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/ciwc_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/clwc_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/hus_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/huss_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/o3_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/ps_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/ssrd_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/ssru_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/ta_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/tisr_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/ts_base.dat
+# ln -s /home/yangsong3/huxm/antarctic_melt/base_no_cloud_out_1.dat
 
+for (( i = 1; i < 63; i++ )); do
+	ln -s /home/yangsong3/huxm/antarctic_melt/baseline_radranc_$i.grd
+	ln -s /home/yangsong3/huxm/antarctic_melt/baseline_radranc_$i.grd
+
+done
