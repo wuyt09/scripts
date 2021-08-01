3710a9437d48e011093b4872278d04667fb3b6dd
wuyt09 <yangsong3@mail.sysu.edu.cn>
2021-08-02 02:01:03 +0800

    210802

--- Antarctic_melt/9192/soft.sh
+++ Antarctic_melt/9192/soft.sh
@@ -17,6 +17,6 @@
 
 for (( i = 1; i < 63; i++ )); do
 	ln -s /home/yangsong3/huxm/antarctic_melt/baseline_radranc_$i.grd
-	ln -s /home/yangsong3/huxm/antarctic_melt/baseline_radranc_$i.grd
+	# ln -s /home/yangsong3/huxm/antarctic_melt/baseline_radranc_$i.grd
 
 done
